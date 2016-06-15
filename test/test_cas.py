5f30f8c5d86bf5f0f8fa006f65ffa93e6b9e881e
Marcel Lauhoff <ml@irq0.org>
2016-06-15 19:26:38 +0200

    Tests: litter, remove, multiple versions

--- test/test_cas.py
+++ test/test_cas.py
@@ -4,9 +4,9 @@
 
 from StringIO import StringIO
 
-from nose.tools import eq_ as eq
+from nose.tools import eq_ as eq, assert_raises
 
-from rados import Rados
+from rados import Rados, ObjectNotFound
 
 from vaceph.cas import CAS
 from vaceph.chunk import Chunker
@@ -34,6 +34,25 @@ def setup_module():
     ioctx_index = rados.open_ioctx(pool_name)
 
 
+def test_chunker_no_litter():
+    cas = CAS(ioctx_cas)
+    chunker = Chunker(cas, ioctx_index)
+
+    data_in = StringIO(random_bytes(chunker.chunk_size*4))
+    obj_name = random_id()
+
+    chunker.write_full(obj_name, data_in)
+    chunker.remove_all_versions(obj_name)
+
+    cas_objs = [x.key for x in ioctx_cas.list_objects()]
+    index_objs = [x.key for x in ioctx_index.list_objects()]
+
+    print "CAS objects left:", cas_objs
+    print "Index objects left:", index_objs
+
+    eq(len(cas_objs), 0)
+    eq(len(index_objs), 0)
+
 def test_cas_put_get():
     cas = CAS(ioctx_cas)
 
@@ -56,6 +75,7 @@ def test_chunker_put_get_single():
     chunker.read_full(obj_name, data_out, version)
 
     eq(data_in.getvalue(), data_out.getvalue())
+
 
 
 def test_chunker_put_get_multiple():
@@ -86,3 +106,43 @@ def test_chunker_versions():
 
     eq(version, chunker.head_version(obj_name))
     eq(version, chunker.versions(obj_name)[0])
+
+def test_chunker_multiple_versions():
+    cas = CAS(ioctx_cas)
+    chunker = Chunker(cas, ioctx_index)
+
+    data_in = random_bytes(42)
+    obj_name = random_id()
+
+    versions = (
+        chunker.write_full(obj_name, StringIO(data_in)),
+        chunker.write_full(obj_name, StringIO(data_in)),
+        chunker.write_full(obj_name, StringIO(data_in)),
+        chunker.write_full(obj_name, StringIO(data_in)),
+        chunker.write_full(obj_name, StringIO(data_in)),
+    )
+
+    eq(len(versions), len(chunker.versions(obj_name)))
+
+    eq(versions[-1], chunker.head_version(obj_name))
+    eq(versions[0], chunker.versions(obj_name)[0])
+
+def test_chunker_remove():
+    cas = CAS(ioctx_cas)
+    chunker = Chunker(cas, ioctx_index)
+
+    data_in = random_bytes(42)
+    obj_name = random_id()
+
+    version = chunker.write_full(obj_name, StringIO(data_in))
+    chunker.remove_version(obj_name, version)
+
+    # object gone
+    eq(chunker.head_version(obj_name), None)
+
+    chunker.write_full(obj_name, StringIO(data_in))
+    chunker.write_full(obj_name, StringIO(data_in))
+    chunker.remove_all_versions(obj_name)
+
+    # object gone
+    assert_raises(ObjectNotFound, chunker.head_version, obj_name)
