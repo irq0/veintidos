b40fc2e3447977747315cf584b7f601db97227cc
Marcel Lauhoff <lauhoff@uni-mainz.de>
2016-07-12 17:07:52 +0200

    cmdline: Cleanup, Add loglevel to args

--- vaceph.py
+++ vaceph.py
@@ -2,37 +2,35 @@
 # -*- coding: utf-8 -*-
 # ✓
 
-from StringIO import StringIO
-
+import logging
 import argparse
 
-from rados import Rados, ObjectNotFound, ObjectExists
+from rados import Rados, ObjectExists
 
 from vaceph.cas import CAS as CAS_
 from vaceph.chunk import Chunker
 
 RADOS = None
-POOL_NAME = "vaceph"
 CAS = None
 CHUNKER = None
 IOCTX_CAS = None
 IOCTX_INDEX = None
 
-def setup_rados():
+
+def setup_rados(args):
     global RADOS
     RADOS = Rados(conffile='')
     RADOS.connect()
 
-    global POOL_NAME
     try:
-        RADOS.create_pool(POOL_NAME)
+        RADOS.create_pool(args.pool)
     except ObjectExists:
         pass
 
     global IOCTX_CAS
     global IOCTX_INDEX
-    IOCTX_CAS = RADOS.open_ioctx(POOL_NAME)
-    IOCTX_INDEX = RADOS.open_ioctx(POOL_NAME)
+    IOCTX_CAS = RADOS.open_ioctx(args.pool)
+    IOCTX_INDEX = RADOS.open_ioctx(args.pool)
 
     global CAS
     global CHUNKER
@@ -65,39 +63,59 @@ def cmd_rm(args):
     else:
         CHUNKER.remove_version(args.name, args.version)
 
+
 def cmd_get(args):
     CHUNKER.read_full(args.name, args.file, args.version)
 
 
-def main():
+def parse_cmdline():
     parser = argparse.ArgumentParser(prog="vaceph client")
+    parser.add_argument("--pool", type=str, default="vaceph1", help="Ceph pool")
+    parser.add_argument("--debug", action="store_const", dest="loglevel",
+                        const=logging.DEBUG)
+    parser.add_argument("--verbose", action="store_const", dest="loglevel",
+                        const=logging.INFO)
 
     subparsers = parser.add_subparsers()
 
-    ls_parser = subparsers.add_parser("ls")
+    ls_parser = subparsers.add_parser(
+        "ls", help="List CAS and INDEX objects")
     ls_parser.set_defaults(func=cmd_ls)
 
-    put_parser = subparsers.add_parser("put")
+    put_parser = subparsers.add_parser(
+        "put", help="Store file to CAS pool and write INDEX")
     put_parser.add_argument('name', type=str)
     put_parser.add_argument('file', type=argparse.FileType('r'))
     put_parser.set_defaults(func=cmd_put)
 
-    rm_parser = subparsers.add_parser("rm")
+    rm_parser = subparsers.add_parser(
+        "rm", help="Remove file")
     rm_parser.add_argument('name', type=str)
     rm_parser.add_argument('--version', type=str, default="ALL")
     rm_parser.set_defaults(func=cmd_rm)
 
-    get_parser = subparsers.add_parser("get")
+    get_parser = subparsers.add_parser(
+        "get", help="Retrieve file from CAS pool")
     get_parser.add_argument('name', type=str)
     get_parser.add_argument('file', type=argparse.FileType('w'))
     get_parser.add_argument('--version', type=str, default="HEAD")
     get_parser.set_defaults(func=cmd_get)
 
+    return parser.parse_args()
 
-    args = parser.parse_args()
 
-    setup_rados()
+def setup_logging(args):
+    logging.basicConfig(level=args.loglevel,
+                        format=""
+                        "%(asctime)-15s %(levelname)-9s"
+                        "%(threadName)-12s %(name)-15s %(filename)s:%(lineno)s -> %(funcName)s():"
+                        "\n\t"
+                        "%(message)s")
 
+def main():
+    args = parse_cmdline()
+    setup_logging(args)
+    setup_rados(args)
     args.func(args)
 
 if __name__ == "__main__":
